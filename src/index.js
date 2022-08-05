196ee415ca8883180b0ee1f5f99a9c622a6093e1
benjaminkaygisiz <benjamin.kaygisiz@alyce.fr>
2022-08-05 11:00:15 +0200

    configuring nodemon with babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import express from 'express';
 
-console.log("hello");
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World! How you doing today ?');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
